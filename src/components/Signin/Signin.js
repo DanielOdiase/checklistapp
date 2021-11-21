2250d3d153878a08bc32b053f8aea99977dd1654
Daniel Odiase <82052211+DanielOdiase@users.noreply.github.com>
2021-11-20 19:51:57 -0500

    node changes

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -78,7 +78,7 @@ export default function SignIn() {
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
-          <Box component="form"  onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <Box component="form"  disabled={loading} onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
             <TextField
               margin="normal"
               required
