cc8d83cabff8029641e8b05f52fc08cf3825f7fb
Daniel Odiase <82052211+DanielOdiase@users.noreply.github.com>
2021-12-28 01:44:50 -0700

    Update RightBar

--- src/components/Rightbar/Righbar.js
+++ src/components/Rightbar/Righbar.js
@@ -1,7 +1,7 @@
 import React,{useState,useEffect} from 'react'
 import "./Rightbar.css"
 import TextField from '@mui/material/TextField';
-import { Calendar,dateFnsLocalizer }from "react-big-calendar/lib";
+import { Calendar,dateFnsLocalizer }from "react-big-calendar";
 import format from "date-fns/format"
 import parse from "date-fns/parse"
 import startOfWeek from 'date-fns/startOfWeek'
